cac8fc9d9a646dec4140802b7d2a457e79b8e2e5
Ally Summers <ally.summers@outlook.com.au>
2024-03-24 00:35:39 +1100

    Update eslint config and prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -5,6 +5,37 @@ module.exports = {
     "prettier-plugin-tailwindcss",
   ],
   tailwindFunctions: ["clsx", "cn", "twmerge", "cva"],
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  editorconfig: true,
+  endOfLine: "lf",
+  jsonRecursiveSort: true,
+  overrides: [
+    {
+      files: ".prettierrc",
+      options: { parser: "json" },
+    },
+    {
+      files: [".prettierrc", "*.json", ".*.json"],
+      options: {
+        printWidth: 80,
+      },
+    },
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
+  plugins: ["prettier-plugin-package-perfection", "prettier-plugin-sort-json"],
+  printWidth: 100,
+  quoteProps: "as-needed",
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "none",
   importOrder: [
     "^(react/(.*)$)|^(react$)",
     "^(next/(.*)$)|^(next$)",
